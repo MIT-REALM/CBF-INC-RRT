7be07adb3d392c36b0c9fe27d93749e2b60c04b6
Mingxin Yu <ymxplus@gmail.com>
2024-09-16 11:00:44 -0400

    update imports

--- motion_planning/evaluation/__init__.py
+++ motion_planning/evaluation/__init__.py
@@ -3,11 +3,6 @@ from .eval_batchrrt_vanilla import eval_batchrrt_vanilla
 from .eval_rrt_mindis_cbfsteer import eval_rrt_mindis
 from .eval_rrt_lidar_cbfsteer import eval_rrt_lidar
 from .eval_batchrrt_mindis_cbfsteer import eval_batchrrt_mindis
-from .eval_rrt_mindis_ilsteer import eval_rrt_mindis_IL
-from .eval_rrt_mindis_rlsteer import eval_rrt_mindis_RL
-from .eval_rrt_lidar_ilsteer import eval_rrt_lidar_IL
-from .eval_rrt_lidar_rlsteer import eval_rrt_lidar_RL
-from .eval_rrt_mindis_optrlsteer import eval_rrt_mindis_OptRL
 from .eval_rrt_dd import eval_rrt_dd
 # from .eval_rrt_docbf import eval_rrt_docbf
 
@@ -18,11 +13,6 @@ __all__ = [
 	'eval_rrt_lidar',
 	'eval_batchrrt_mindis',
 	'eval_batchrrt_vanilla',
-	'eval_rrt_mindis_IL',
-	'eval_rrt_mindis_RL',
-	'eval_rrt_lidar_IL',
-	'eval_rrt_lidar_RL',
-	'eval_rrt_mindis_OptRL',
 	'eval_rrt_dd',
 	# 'eval_rrt_docbf',
 ]
